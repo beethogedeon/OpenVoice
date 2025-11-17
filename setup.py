420e76a5384abe87db95f2c5b3bd5c3c0945ad7f
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2025-11-17 16:48:40 +0100

    Refactor setup.py for OpenVoice package
    Updated package name and version, modified author details, and changed some dependencies.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 
-setup(name='MyShell-OpenVoice',
-      version='0.0.0',
-      description='Instant voice cloning by MyShell.',
+setup(name='OpenVoice',
+      version='0.1.0',
+      description='Instant voice cloning by MyShell - Enhanced by Gedeon.',
       long_description=open('README.md').read().strip(),
       long_description_content_type='text/markdown',
       keywords=[
@@ -12,34 +12,37 @@ setup(name='MyShell-OpenVoice',
             'voice-clone',
             'zero-shot-tts'
       ],
-      url='https://github.com/myshell-ai/OpenVoice',
+      url='https://github.com/beethogedeon/OpenVoice',
       project_urls={
-        'Documentation': 'https://github.com/myshell-ai/OpenVoice/blob/main/docs/USAGE.md',
-        'Changes': 'https://github.com/myshell-ai/OpenVoice/releases',
-        'Code': 'https://github.com/myshell-ai/OpenVoice',
-        'Issue tracker': 'https://github.com/myshell-ai/OpenVoice/issues',
+        'Documentation': 'https://github.com/beethogedeon/OpenVoice/blob/main/docs/USAGE.md',
+        'Changes': 'https://github.com/beethogedeon/OpenVoice/releases',
+        'Code': 'https://github.com/beethogedeon/OpenVoice',
+        'Issue tracker': 'https://github.com/beethogedeon/OpenVoice/issues',
       },
-      author='MyShell',
-      author_email='ethan@myshell.ai',
+      author=['MyShell, Gedeon GBEDONOU'],
+      author_email=['ethan@myshell.ai','beethovengedeon@gmail.com'],
       license='MIT License',
       packages=find_packages(),
 
       python_requires='>=3.9',
       install_requires=[
-            'librosa==0.9.1',
-            'faster-whisper==0.9.0',
-            'pydub==0.25.1',
-            'wavmark==0.0.3',
-            'numpy==1.22.0',
-            'eng_to_ipa==0.0.2',
-            'inflect==7.0.0',
-            'unidecode==1.3.7',
-            'whisper-timestamped==1.14.2',
-            'pypinyin==0.50.0',
-            'cn2an==0.5.22',
-            'jieba==0.42.1',
-            'gradio==3.48.0',
-            'langid==1.1.6'
+            'librosa==0.11.0',
+'git+https://github.com/SYSTRAN/faster-whisper',
+'pydub==0.25.1',
+'wavmark',
+'numpy',
+'eng_to_ipa',
+'inflect',
+'unidecode',
+'whisper-timestamped',
+'openai',
+'python-dotenv',
+'pypinyin',
+'cn2an',
+'jieba',
+'gradio',
+'langid'
       ],
       zip_safe=False
       )
+
